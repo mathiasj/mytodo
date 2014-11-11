7b77f2ba775f9d479604114bb4ac2131fe75c44e
Mathias Johansson <hej@mathiasjohansson.se>
2014-11-11 20:24:33 +0100

    Step 8

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -17,19 +17,22 @@ module.exports = function(config) {
     frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
+      files: [
+          'bower_components/angular/angular.js',
+          'bower_components/angular-mocks/angular-mocks.js',
+          'bower_components/angular-animate/angular-animate.js',
+          'bower_components/angular-cookies/angular-cookies.js',
+          'bower_components/angular-resource/angular-resource.js',
+          'bower_components/angular-route/angular-route.js',
+          'bower_components/angular-sanitize/angular-sanitize.js',
+          'bower_components/angular-touch/angular-touch.js',
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/jquery-ui/ui/jquery-ui.js',
+          'bower_components/angular-ui-sortable/sortable.js',
+          'app/scripts/**/*.js',
+          'test/mock/**/*.js',
+          'test/spec/**/*.js'
+      ],
 
     // list of files / patterns to exclude
     exclude: [],
